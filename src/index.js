b1bae7ab124f7294adbb407b953d280a78e071a3
lucas <lucasff15@hotmail.com>
2023-02-11 01:44:56 -0300

    modified:   package-lock.json 	modified:   package.json 	deleted:    public/favicon.ico 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    src/App.css 	deleted:    src/App.js 	deleted:    src/App.test.js 	new file:   src/img/08_day_changing_background_exercise.gif 	new file:   src/img/lucas.jpg 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js deleted:    src/setupTests.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,177 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import lucasImage from "./img/lucas.jpg";
+import { BsCheckCircleFill } from "react-icons/bs";
+import { AiOutlineLeftCircle } from "react-icons/ai";
+import { BsCircleHalf } from "react-icons/bs";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//Header component
+//class component
+class Header extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  render() {
+    const { firstName, lastName, occupation, country, image } = this.props.data;
+    return (
+      <header className="headerWrapper">
+        <div className="imgWrapper">
+          <img src={image} alt={firstName}></img>
+        </div>
+        <div className="nameLabel">
+          <h1>
+            {firstName} {lastName}
+          </h1>
+          <BsCheckCircleFill id="verify" />
+        </div>
+        <div className="location">
+          <p>
+            {occupation}, {country}
+          </p>
+        </div>
+      </header>
+    );
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// TechList Component
+//class component
+class TechList extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  render() {
+    const { techs } = this.props;
+    const techList = techs.map((tech) => <span key={tech}>{tech}</span>);
+    return techList;
+  }
+}
+
+//Main Module
+//class component
+class Main extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  render() {
+    const { techs } = this.props;
+    return (
+      <main>
+        <h2>SKILLS</h2>
+        <div>
+          <TechList techs={techs} />
+        </div>
+      </main>
+    );
+  }
+}
+
+//Footer module
+//class component
+class Footer extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  render() {
+    const { date, showDate, changeBackground } = this.props;
+    return (
+      <footer>
+        <AiOutlineLeftCircle />
+        <small>Joinned {showDate(date)}</small>
+        <BsCircleHalf id="darkMode" onClick={changeBackground} />
+      </footer>
+    );
+  }
+}
+
+//App Module
+//class component
+class App extends React.Component {
+  state = {
+    styles: {
+      backgroundColor: "#fff",
+      color: "#222",
+    },
+  };
+  showDate = (time) => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const month = months[time.getMonth()].slice(0, 3);
+    const year = time.getFullYear();
+    const date = time.getDate();
+    return ` ${month} ${date}, ${year}`;
+  };
+  changeBackground = () => {
+    this.setState((prevState) => {
+      if (prevState.styles.backgroundColor === "#fff") {
+        return {
+          styles: {
+            backgroundColor: "#222",
+            color: "#dfdfdf",
+          },
+        };
+      } else {
+        return {
+          styles: {
+            backgroundColor: "#fff",
+            color: "#222",
+          },
+        };
+      }
+    });
+  };
+  render() {
+    const date = new Date();
+    const data = {
+      firstName: "Lucas",
+      lastName: "Faria",
+      occupation: "Software Developer",
+      country: "Brazil",
+      image: lucasImage,
+    };
+    const techs = [
+      "HTML",
+      "CSS",
+      "JS",
+      "React",
+      "Redux",
+      "Node",
+      "MongoDB",
+      "Python",
+      "Flask",
+      "Django",
+      "MYSQL",
+      "GraphQL",
+      "D3.js",
+      "Git",
+    ];
+    return (
+      <div className="appWrapper" style={this.state.styles}>
+        <Header data={data} />
+        <Main techs={techs} />
+        <Footer
+          changeBackground={this.changeBackground}
+          date={date}
+          showDate={this.showDate}
+        />
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.createRoot(rootElement).render(<App />);
